13c3bab7d68440f4fdb0ccb22445b1028fcfa2a0
Ezequiel Llado <ezequiel.llado.ce@gmail.com>
2021-05-06 10:50:36 +0200

    update

---
+++ Configure_Basic_infra/Public_ip_from_backend.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+data "terraform_remote_state" "bk_end" {
+  backend = "s3"
+  config = {
+      bucket = "backendbucket20210505"
+      key    = "data_front/"
+      region = "us-east-2"
+  }
+}
+
+output "pub_ip" {
+  value = data.terraform_remote_state.bk_end.outputs.pb_ip
+}
