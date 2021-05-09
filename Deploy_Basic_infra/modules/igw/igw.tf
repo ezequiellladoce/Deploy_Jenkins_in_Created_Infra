17f2a2cbda76410bc75c2b1a17536e25004ad537
Ezequiel Llado <ezequiel.llado.ce@gmail.com>
2021-05-09 20:34:08 +0200

    route table vars

--- Deploy_Basic_infra/modules/igw/igw.tf
+++ Deploy_Basic_infra/modules/igw/igw.tf
@@ -12,6 +12,6 @@ resource "aws_route_table" "rtb_public" {
 }
 
 resource "aws_route_table_association" "rta_subnet_public" {
-  subnet_id      = aws_subnet.subnet_public.id
-  route_table_id = aws_route_table.subnet.id
+  subnet_id      = var.snet_id
+  route_table_id = aws_route_table.rtb_public.id
 }
